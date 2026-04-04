7edd4d935ec9b8599dac20dfbac9f96eac095214
Assics99 <razvozzaevivan@gmail.com>
2026-04-04 14:57:12 +0900

    Теперь скрипт может публиковать фото раз в несколько часов

--- tg_bot.py
+++ tg_bot.py
@@ -1,14 +1,33 @@
+import os
+import time
+import argparse
 import telegram
 
 BOT_TOKEN = os.environ['TG_KEY']
 CHAT_ID = "@space_photos88"
-PHOTO_PATH = "photo.jpg"
 
-def send_photo():
+def get_photos(folder):
+    exts = (".jpg", ".jpeg", ".png", ".webp")
+    return [os.path.join(folder, f) for f in os.listdir(folder) if f.lower().endswith(exts)]
+
+def send_photos(bot, photos):
+    for photo_path in photos:
+        with open(photo_path, "rb") as photo:
+            bot.send_photo(chat_id=CHAT_ID, photo=photo)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("folder", help="Папка с фото")
+    parser.add_argument("interval", type=float, help="Интервал в часах")
+    args = parser.parse_args()
+
     bot = telegram.Bot(token=BOT_TOKEN)
-    
-    with open(PHOTO_PATH, "rb") as photo:
-        bot.send_photo(chat_id=CHAT_ID, photo=photo)
+
+    while True:
+        photos = get_photos(args.folder)
+        if photos:
+            send_photos(bot, photos)
+        time.sleep(args.interval * 3600)
 
 if __name__ == "__main__":
-    send_photo()
+    main()
