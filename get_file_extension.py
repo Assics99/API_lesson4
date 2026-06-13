7493677f077dbcb56925e254f97b6542da37bb9a
Assics99 <razvozzaevivan@gmail.com>
2026-06-13 14:12:31 +0900

    Delete get_file_extension.py
    Избавился от дубликатов

--- get_file_extension.py
+++
@@ -1,9 +0,0 @@
-import os
-from urllib.parse import urlparse
-
-
-def get_file_extension(url):
-    path = urlparse(url).path
-    result = os.path.splitext(path)
-    extension = result[1]
-    return extension
